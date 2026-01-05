70fd9216ce2228d1c55a09e37a3d7b39eb81f55b
wondavibes <amosayemoro2001@gmail.com>
2026-01-05 11:08:14 +0100

    snippets code api using httpie

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "quickstart",
     "snippets",
 ]
@@ -126,4 +128,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    #"DEFAULT_AUTHENTICATION_CLASSES": (
+    #    "rest_framework_simplejwt.authentication.JWTAuthentication",
+    #   ),
 }
