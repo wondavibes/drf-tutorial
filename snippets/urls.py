731883aefedf30b566a117d49b6dc2b545216998
wondavibes <amosayemoro2001@gmail.com>
2026-01-07 01:10:37 +0100

    added format for context based urls

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import snippet_list, snippet_detail
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("snippets/", snippet_list),
     path("snippets/<int:pk>/", snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
